0efd2f5b15584206f61be469e60a792c81e9f872
AdrianCatrinoiu <62521597+AdrianCatrinoiu@users.noreply.github.com>
2022-03-16 01:13:07 +0200

    Revert "Added new entity: OrderEntity and corresponding repository. (#3)"
    This reverts commit 1f0e985f60dbef74f6025618ef9401fb9532244c.

--- src/test/java/ro/unibuc/hello/HelloApplicationTests.java
+++ src/test/java/ro/unibuc/hello/HelloApplicationTests.java
@@ -4,16 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ro.unibuc.hello.data.ArtworkRepository;
-import ro.unibuc.hello.data.OrderRepository;
 
 @SpringBootTest
 class HelloApplicationTests {
 
 	@MockBean
 	ArtworkRepository mockRepository;
-
-	@MockBean
-	OrderRepository mockOrderRepository;
 
 	@Test
 	void contextLoads() {
